98e33d30447d3c386da01a12e9127c0364ac3b5d
hekaku <hekakui@phreetech.com>
2024-07-02 22:10:20 +0100

    updates

---
+++ src/app/confirmemail/confirmemail.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
+
+@Component({
+  selector: 'app-confirmemail',
+  standalone: true,
+  imports: [FooterComponent],
+  templateUrl: './confirmemail.component.html',
+  styleUrl: './confirmemail.component.css'
+})
+export class ConfirmemailComponent {
+
+}
