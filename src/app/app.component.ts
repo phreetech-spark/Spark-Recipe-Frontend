518beb885acbae3a3de36f1d28a548bd6fe4f56e
michael__a <michael.a@phreetech.com>
2024-07-02 16:20:32 +0100

    ui update for tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeroComponent } from './hero/hero.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FooterComponent,HeroComponent,RouterModule],
+  imports: [RouterOutlet,FooterComponent, NavbarComponent, HeroComponent,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
