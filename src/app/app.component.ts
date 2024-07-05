9ea70b595636131fe57df2c9e379def620d120ee
hekaku <hekakui@phreetech.com>
2024-07-05 14:54:47 +0100

    my second update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeroComponent } from './hero/hero.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FooterComponent,HeroComponent,RouterModule],
+  imports: [RouterOutlet,FooterComponent,HeroComponent,RouterModule,NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
